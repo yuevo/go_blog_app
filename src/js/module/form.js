fbe29b91279409977dc739c4ce138be9b89152c3
yuevo <yuevo16@gmail.com>
2021-04-27 18:23:24 +0900

    バリデーション追加

--- src/js/module/form.js
+++ src/js/module/form.js
@@ -13,6 +13,8 @@ document.addEventListener('DOMContentLoaded', () => {
   const articleFormPreview = document.querySelector('.article-form__preview');
   const articleFormBodyTextArea = document.querySelector('.article-form__input--body');
   const articleFormPreviewTextArea = document.querySelector('.article-form__preview-body-contents');
+  const errors = document.querySelector('.article-form__errors');
+  const errorTmpl = document.querySelector('.article-form__error-tmpl').firstElementChild;
 
   // 新規作成画面か編集画面かを URL から判定します。
   const mode = { method: '', url: '' };
@@ -50,6 +52,9 @@ document.addEventListener('DOMContentLoaded', () => {
   saveBtn.addEventListener('click', event => {
     event.preventDefault();
 
+    // 前回のバリデーションエラーの表示が残っている場合は削除します。
+    errors.innerHTML = null;
+
     // フォームに入力された内容を取得します。
     const fd = new FormData(form);
 
@@ -75,10 +80,40 @@ document.addEventListener('DOMContentLoaded', () => {
 
         if (body.ValidationErrors) {
           // バリデーションエラーがある場合の処理をここに記載します。
+          showErrors(body.ValidationErrors);
         }
       })
       .catch(err => console.error(err));
   });
+
+  // バリデーションエラーを表示する関数
+  const showErrors = messages => {
+    console.log("test");
+    console.log(messages);
+    // 引数の値が配列であることを確認します。
+    if (Array.isArray(messages) && messages.length != 0) {
+      // 複数メッセージを格納するためのフラグメントを作成します。
+      const fragment = document.createDocumentFragment();
+
+      // メッセージをループ処理します。
+      messages.forEach(message => {
+        // 単一メッセージを格納するためのフラグメントを作成します。
+        const frag = document.createDocumentFragment();
+
+        // テンプレートをクローンしてフラグメントに追加します。
+        frag.appendChild(errorTmpl.cloneNode(true));
+
+        // エラー要素にメッセージをセットします。
+        frag.querySelector('.article-form__error').innerHTML = message;
+
+        // エラー要素を親フラグメントに追加します。
+        fragment.appendChild(frag);
+      });
+
+      // エラーメッセージの表示エリア（要素）にメッセージを追加します。
+      errors.appendChild(fragment);
+    }
+  };
 
   // プレビューを開くイベントを設定します。
   previewOpenBtn.addEventListener('click', event => {
